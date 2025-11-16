2f5bf3c1bf9a4d957e95714dab75f7077b1f651e
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-11-16 22:35:41 +0900

    TODO: complete parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    
+use clap::Parser;
+use union_package_manager::modules::cmd::Cli;
+use union_package_manager::modules::error::UpmError;
+fn main()->Result<(),UpmError> {
+    let cmd = Cli::parse();
+    cmd.execute()
 }
--- src/modules/cmd.rs
+++ src/modules/cmd.rs
@@ -1,3 +1,29 @@
-enum Command{
-    
+use clap::Parser;
+use clap::Subcommand;
+
+use crate::modules::error::UpmError;
+#[derive(Debug, Parser)]
+#[clap(
+    name = env!("CARGO_PKG_NAME"),
+    version = env!("CARGO_PKG_VERSION"),
+    author = env!("CARGO_PKG_AUTHORS"),
+    about = env!("CARGO_PKG_DESCRIPTION"),
+    arg_required_else_help = true,
+)]
+pub struct Cli {
+    #[clap(subcommand)]
+    subcommand: SubCommands,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum SubCommands {
+    Get,
+    Post,
+}
+
+impl Cli {
+    pub fn execute(&self)->Result<(),UpmError>{
+        return Ok(())
+    }
+
 }
